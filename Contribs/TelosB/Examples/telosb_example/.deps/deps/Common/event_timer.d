e1cd7452349d4e6b735ecf3787021c12d74c75f2
zdshelby <zdshelby@6b27bb7e-ec42-0410-a66d-b8668be7df7d>
2008-04-10 08:07:35 +0000

    Cleanup of build directories.
    git-svn-id: svn://svn.code.sf.net/p/nanostack/code@2 6b27bb7e-ec42-0410-a66d-b8668be7df7d

--- Contribs/TelosB/Examples/telosb_example/.deps/deps/Common/event_timer.d
+++
@@ -1,9 +0,0 @@
-.objs/build/root/maxfor_example/../../Common/event_timer.o: ../../Common/event_timer.c \
-  ../../FreeRTOS/Source/include/FreeRTOS.h \
-  ../../FreeRTOS/Source/include/projdefs.h FreeRTOSConfig.h \
-  ../../FreeRTOS/Source/include/portable.h \
-  ../../FreeRTOS/Source/portable/GCC/MSP430F449/portmacro.h \
-  ../../FreeRTOS/Source/include/task.h \
-  ../../FreeRTOS/Source/include/list.h \
-  ../../FreeRTOS/Source/include/queue.h \
-  ../../Common/include/event_timer.h
--- Contribs/TelosB/Examples/telosb_example/.deps/deps/Platform/maxfor810cm/powersave.d
+++
@@ -1,1 +0,0 @@
-.objs/build/root/maxfor_example/../../Platform/maxfor810cm/powersave.o: ../../Platform/maxfor810cm/powersave.c
